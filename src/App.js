191538e36febf1a83e902018bdf14c5403404177
mstars_lab2_08 <mstars_lab2_08@mstars-lab2-08s-iMac.local>
2022-03-25 15:45:14 +0800

    hey

--- src/App.js
+++ src/App.js
@@ -15,17 +15,24 @@ class App extends Component {
       "http://api.giphy.com/v1/gifs/trending?api_key=7AH1655auFDNR3kT7aSJfxvP7nfezgrj"
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        this.setState({ gifs: data.data });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
   }
   render() {
+    console.log(this.state.gifs);
     return (
       <div className="mean-header">
         <div className="inner">
           <h1 className="main-title">Search form</h1>
           <SearchForm />
         </div>
-        <div className="main-content"></div>
-        <GifList />
+        <div className="main-content">
+          <GifList data={this.state.gifs} />
+        </div>
       </div>
     );
   }
